9c1374a904573a3032bb8be2c1f9fd6c61add625
Cayden de Wit <cayden@4dewits.com>
2021-01-26 18:20:25 +0200

    Refactored some code. Added some formating for printing. Added a playlist class for later playlist making. Made 2 config files for json

--- musicplayer.py
+++ musicplayer.py
@@ -1,6 +1,9 @@
 try:
    import os
    import json
+   import fcntl
+   import struct
+   import termios
    import argparse
    import subprocess
 
@@ -18,6 +21,16 @@ volume = None
 results = None
 clearbool = None
 videobool = None
+
+class playlistfuncs:
+   def newsong(url, title, channel, duration, views):
+      return {"url": url, "title": title, "channel": channel, "duration": duration, "views": views}
+
+   def newplaylist(name):
+      return {"name": name, "songs": []}
+
+   def newplaycollection():
+      return {"playlists": []}
 
 def mainloop():
    global resarray
@@ -53,12 +66,18 @@ def mainloop():
             viewarray = tmpresarray['views']
             durationarray = tmpresarray['durations']
          else:
-            tmpresarray = dosearchyt(input("Input search term > "), results)
-            resarray = tmpresarray['urls']
-            titlearray = tmpresarray['titles']
-            channelarray = tmpresarray['channels']
-            viewarray = tmpresarray['views']
-            durationarray = tmpresarray['durations']
+            inp = input("Input search term > ")
+
+            if (inp != ""):
+               tmpresarray = dosearchyt(inp, results)
+               
+               resarray = tmpresarray['urls']
+               titlearray = tmpresarray['titles']
+               channelarray = tmpresarray['channels']
+               viewarray = tmpresarray['views']
+               durationarray = tmpresarray['durations']
+            else:
+               print("Nothing searched!")
       
       elif (cmd == '/'):
          if (len(cmdinput) > 1):
@@ -70,12 +89,18 @@ def mainloop():
             viewarray = tmpresarray['views']
             durationarray = tmpresarray['durations']
          else:
-            tmpresarray = dosearchyt(input("Input search term > "), results)
-            resarray = tmpresarray['urls']
-            titlearray = tmpresarray['titles']
-            channelarray = tmpresarray['channels']
-            viewarray = tmpresarray['views']
-            durationarray = tmpresarray['durations']
+            inp = input("Input search term > ")
+            
+            if (inp != ""):
+               tmpresarray = dosearchyt(inp, results)
+               
+               resarray = tmpresarray['urls']
+               titlearray = tmpresarray['titles']
+               channelarray = tmpresarray['channels']
+               viewarray = tmpresarray['views']
+               durationarray = tmpresarray['durations']
+            else:
+               print("Nothing searched!")
 
       elif (cmd == 'p'):
          if (dotestmusic): dostopmusic()
@@ -87,20 +112,24 @@ def mainloop():
 
             doplay(resarray[int(cmdinput[1]) - 1])
          except IndexError:
-            inp1 = input("Input video number > ")
-            inp = resarray[int(inp1) - 1]
+            try:
+               inp1 = input("Input video number > ")
+               inp = resarray[int(inp1) - 1]
 
-            currplay = titlearray[int(inp1) - 1]
+               currplay = titlearray[int(inp1) - 1]
 
-            doplay(inp)
+               doplay(inp)
+            except IndexError:
+               print("No such video!")
          except NameError:
             print("You have not searched anything yet!")
 
       elif (cmd == 'i'):
          i = 0
+         tw = int(terminal_size()[0])
 
          try:
-            if (currplay == ""):
+            if (currplay == "" or dotestmusic() == False):
                print("Nothing playing\n")
             else:
                print("Now playing: " + currplay)
@@ -109,8 +138,10 @@ def mainloop():
 
          try:
             for i in range (len(titlearray)):
+               formated = '%-{a}s %-{b}s %-{c}s %-{d}s'.format(a=int(tw*0.54), b=int(tw*0.2), c=int(tw*0.13), d=int(tw*0.10))
+
                print(i+1, end='')
-               print('. %-80s %-30s %-20s %-16s' % (titlearray[i], channelarray[i], viewarray[i], durationarray[i]))
+               print('. ' + formated % (titlearray[i], channelarray[i], viewarray[i], durationarray[i]))
          except NameError:
             print("Nothing searched!")
       
@@ -139,6 +170,9 @@ def mainloop():
       elif (cmd == 'q'):
          doexit()
 
+      else:
+         print("Invalid command!")
+
 def doserialize(inp):
    return json.dumps(inp, indent=3)
 
@@ -146,19 +180,17 @@ def dodeserialize(inp):
    return json.loads(inp)
 
 def dohelp():
-   print("""
-   <Key>   <Function>                                                      <Args>
+   keys = ["h", "s", "i", "q", "?", "/", "p", "v", "r"]
+   func = ["Print this help list", "Stop current song", "Print current info about current song and music stack", "Exit the program",
+   "Search for a specific term and add the results to the stack", "Same as ? (Search for song. Add to stack)", "Play Song",
+   "Set volume in percent ", "Set result amount (Warning more results = slower search) "]
+   args = ["NONE", "NONE", "NONE", "NONE", "Search term (String)", "Search term (String)", "Number of song (Int)", "Int (0 - 100)", "Int"]
 
-   h   -   Print this help list                                            NONE
-   s   -   Stop current song                                               NONE
-   i   -   Print current info about current song and music stack           NONE
-   q   -   Exit the program                                                NONE
-   ?   -   Search for a specific term and add the results to the stack     Search term (String)
-   /   -   Same as ? (Search for song. Add to stack)                       Search term (String)
-   p   -   Play Song                                                       Number of song (Int)
-   v   -   Set volume in percent                                           Int (0 - 100)
-   r   -   Set result amount (Warning more results = slower search)        Int
-   """)
+   tw = terminal_size()[0]
+
+   for i in range(len(keys)):
+      formated = '   %-{a}s %-{b}s %-{c}s'.format(a=int(tw*0.05), b=int(tw*0.7), c=int(tw*0.20))
+      print(formated % (keys[i], func[i], args[i]))
 
 def dotestmusic():
    if (os.popen("ps -e | grep -i mpv") != ""):
@@ -185,6 +217,7 @@ def dosearchyt(query, num):
 
    results = YoutubeSearch(query, max_results=num).to_dict()
 
+
    for i in range(num):
       ids[i] = results[i]['id']
       views[i] = results[i]['views']
@@ -198,19 +231,25 @@ def dosearchyt(query, num):
 
    return datadict
 
+def terminal_size():
+   th, tw, hp, wp = struct.unpack('HHHH',
+      fcntl.ioctl(0, termios.TIOCGWINSZ,
+      struct.pack('HHHH', 0, 0, 0, 0)))
+   return tw, th
+
 def readandcheckconfig():
    try:
-      f = open("pymusicplaylist.conf")
-      playarray = f.read().split('\n')
-      playarray.pop(0)
+      f = open("playlists.json")
    except IOError:
-      print("Welcome to music player.py!\nThis is either your first time using the\nprogram or you have deleted your playlist config file\nBy typing \"OK\", \"Yes\" or \"y\" you agree to let this program\nwrite 1 file(s) to your disk.\nDo you accept?\n")
+      print("Welcome to music player.py!\nThis is either your first time using the\nprogram or you have deleted your playlist config file\nBy typing \"OK\", \"Yes\" or \"y\" you agree to let this program\nwrite to your disk.\nDo you accept?\n")
       confirm = input("Type \"OK, y or yes\" to agree: ")
       
       if (confirm.lower() == "y" or confirm.lower() == "yes" or confirm.lower() == "ok"):
-         f = open("pymusicplaylist.conf", "w+")
+         f = open("playlists.json", "w+")
+         b = open("downloads.json", "w+")
+
          f.close()
-         print("Welcome new user! Type \"h\" and hit enter to see your help list\n")
+         b.close()
       else:
          exit()
 
